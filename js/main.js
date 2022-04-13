ac2b2d1c0d960ba90e20ac4cfd010bf1348dc8ad
Gerard <barros.gc@gmail.com>
2022-04-13 04:47:38 -0500

    Added files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date
+document.querySelector('button').addEventListener('click', getFetch)
+ 
+function getFetch(){
+  const choice = document.querySelector("input").value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=wsf8wSZW8YvSzBd5KHUjz53XjVBCU44CKey6A1PU&date=${choice}`
+  document.querySelector("h3").classList.remove("hidden")
+  fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.media_type === "image"){
+          document.querySelector("img").src = data.url
+        } else if(data.media_type === "video"){
+          document.querySelector("iframe").src = data.url
+          document.querySelector("img").classList.add("hidden")
+        }
+
+        document.querySelector("h2").innerText = data.title
+        
+        document.querySelector(".description").innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
