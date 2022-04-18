fb3d1cfb0e66ee5767e0b04fd6a65e0871db09e4
Gerard <barros.gc@gmail.com>
2022-04-18 01:34:51 -0500

    Updated responsiveness

--- js/main.js
+++ js/main.js
@@ -5,16 +5,20 @@ function getFetch(){
   const choice = document.querySelector("input").value
   const url = `https://api.nasa.gov/planetary/apod?api_key=wsf8wSZW8YvSzBd5KHUjz53XjVBCU44CKey6A1PU&date=${choice}`
   document.querySelector("h3").classList.remove("hidden")
+  
   fetch(url)
       .then(res => res.json())
       .then(data => {
         console.log(data)
         if (data.media_type === "image"){
           document.querySelector("img").src = data.url
+          document.querySelector("img").classList.remove("hidden")
+          document.querySelector("iframe").classList.add("hidden")
         } else if(data.media_type === "video"){
           document.querySelector("iframe").src = data.url
           document.querySelector("img").classList.add("hidden")
-        }
+          document.querySelector("iframe").classList.remove("hidden")
+        } 
 
         document.querySelector("h2").innerText = data.title
         
